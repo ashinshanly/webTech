36603d281be25276234df7f6e8bfbfeb66addd66
ezudheen <ezudheen@gmail.com>
2019-02-06 09:44:43 +0530

    initial commit

---
+++ addTwoNumbers.js
@@ -0,0 +1,21 @@
+function add() {
+
+    	var x = Number(document.getElementById('x').value);
+
+    	var y = Number(document.getElementById('y').value);
+
+    	document.getElementById('result').innerHTML = x+y;
+
+	return false;
+}
+
+function message() {
+
+    	document.getElementById('go').innerHTML = "SUM";
+
+	return false;
+}
+
+document.getElementById('go').addEventListener('click', add);
+
+document.getElementById('go').addEventListener('mouseover', message);
